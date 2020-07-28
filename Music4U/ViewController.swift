447c2fc573359249fcd9a1b3e357e95a4ea77661
Doan Le Minh <dlminh@tma.com.vn>
2020-07-28 16:27:47 +0700

    Initial Commit

---
+++ Music4U/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music4U
+//
+//  Created by Lu Gia Lam on 7/28/20.
+//  Copyright © 2020 Minh Doan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
